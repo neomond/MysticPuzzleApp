43f4fd7aa664d0bfc5145de56d1096309956aae9
neomond <neoweise@yahoo.com>
2024-08-08 16:23:02 +0400

    refactoring

--- MysticPuzzle/MysticPuzzleView.swift
+++ MysticPuzzle/MysticPuzzleView.swift
@@ -13,7 +13,9 @@ struct MysticPuzzleView: View {
     
     let title = "Mystic Puzzle "
     let tileDimensions: CGFloat = 70
+    
     var body: some View {
+        
         ZStack{
             // Background color
             Color(
@@ -24,12 +26,14 @@ struct MysticPuzzleView: View {
                     .fontWeight(.bold)
                     .padding()
                 ZStack {
-                    Text("Tiles")
-                    Image("digit0")
-                        .resizable()
-                        .renderingMode(.template)
-                        .foregroundStyle(.red)
-                        .frame(width: tileDimensions, height: tileDimensions)
+                    let tiles = mysticPuzzleViewModel.mysticPuzzleModel.tiles
+                    
+                    ForEach(tiles) { tile in
+                        TileView(
+                            tileNumber: tile.value,
+                            tileDimensions: tileDimensions,
+                            offset: tile.currentPoint)
+                    }
                 }
             }
         }
@@ -38,4 +42,39 @@ struct MysticPuzzleView: View {
 
 #Preview {
     MysticPuzzleView()
+}
+
+struct TileView: View {
+    let tileNumber: Int
+    let tileDimensions: CGFloat
+    let offset: CGPoint
+    
+    let somePadding: CGFloat = 3
+    let digitResizeFactor: CGFloat = 0.65
+    
+    var body: some View {
+        let direction = offset * tileDimensions
+        
+        let deltaX = direction.x + tileDimensions / 2
+        let deltaY = direction.y - 3 + tileDimensions / 2
+        
+        
+        Image("digit\(tileNumber)")
+            .resizable()
+            .renderingMode(.template)
+            .foregroundStyle(.red)
+            .frame(
+                width: tileDimensions*digitResizeFactor,
+                height: tileDimensions*digitResizeFactor)
+            .padding()
+            .background(
+                Color.gray
+                    .frame(
+                        width: tileDimensions - somePadding,
+                        height: tileDimensions - somePadding)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            )
+            .offset(x: deltaX, y: deltaY)
+            .shadow(color: .black, radius: 1, x: 1, y: 1)
+    }
 }
