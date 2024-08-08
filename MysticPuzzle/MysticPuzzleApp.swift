cd521e789da40052a7fbcf660ebe838d153cc686
neomond <neoweise@yahoo.com>
2024-08-08 11:29:06 +0400

    Initial Commit

---
+++ MysticPuzzle/MysticPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticPuzzleApp.swift
+//  MysticPuzzle
+//
+//  Created by Nazrin Atayeva on 08.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MysticPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
